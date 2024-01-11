f0b0e513987cb5bed9d7b73f0dde0d735bec1d00
Valik <valik3201@gmail.com>
2024-01-11 23:30:39 +0100

    feat(components): add Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import Profile from './Profile.jsx';
+import user from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>React Components</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
